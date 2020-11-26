ae713f3594a5ae170d9f254a0d35d2652b376f16
Nurrochmat Saptoaji <nurrochmat.sapto@gmail.com>
2020-11-26 14:07:47 +0700

    create simple login card
    yang belum ? input sebelumnya harus hilang, validation
    
    reference source: https://www.youtube.com/watch?v=dx-Xss_B7Ig&list=PL9At9z2rvOC-Xkav4tupA8PaXS63TvMnQ&index=12&ab_channel=baledemy

---
+++ simple-login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-card>
+        <v-card-title>Login</v-card-title>
+        <v-form @submit.prevent="handleSubmit">
+          <v-card-text>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle" v-model="user.username"/>
+            <v-text-field label="Password" prepend-icon="mdi-lock" :type="showPassword ? 'text' : 'password'" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click:append="handleClick" v-model="user.password" />
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="success" type="submit">Login</v-btn>
+            <v-btn color="info">Register</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      showPassword: false,
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+
+  methods: {
+    handleClick(){
+      this.showPassword = !this.showPassword
+    },
+    handleSubmit(){
+      console.log(this.user)
+    }
+  },
+
+};
+</script>
